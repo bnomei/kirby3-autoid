dfd33b2caa81f04967a246af636579c08b06913d
Bruno Meilick <b@bnomei.com>
2021-01-22 19:42:04 +0000

    :arrow_up: upgraded dependencies
    closed #58
    
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -8,7 +8,9 @@ $baseDir = dirname($vendorDir);
 return array(
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Ramsey\\Uuid\\' => array($vendorDir . '/ramsey/uuid/src'),
+    'Ramsey\\Collection\\' => array($vendorDir . '/ramsey/collection/src'),
     'Kirby\\' => array($vendorDir . '/getkirby/composer-installer/src'),
     'Hidehalo\\Nanoid\\' => array($vendorDir . '/hidehalo/nanoid-php/src'),
+    'Brick\\Math\\' => array($vendorDir . '/brick/math/src'),
     'Bnomei\\' => array($baseDir . '/classes'),
 );
